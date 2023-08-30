6768c1f284dd9f855b4e5a47f7aa49128f023cde
dineshrajg0648 <128681560+dineshrajg0648@users.noreply.github.com>
2023-08-30 09:15:38 +0530

    Add files via upload

---
+++ Skeleton code/25.java
@@ -0,0 +1,99 @@
+
+
+/**
+ * Class Products
+ */
+public class Products {
+
+  //
+  // Fields
+  //
+
+  private int id;
+  private char Name;
+  private char Group;
+  private char Subgroup;
+  
+  //
+  // Constructors
+  //
+  public Products () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  private void setId (int newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  private int getId () {
+    return id;
+  }
+
+  /**
+   * Set the value of Name
+   * @param newVar the new value of Name
+   */
+  private void setName (char newVar) {
+    Name = newVar;
+  }
+
+  /**
+   * Get the value of Name
+   * @return the value of Name
+   */
+  private char getName () {
+    return Name;
+  }
+
+  /**
+   * Set the value of Group
+   * @param newVar the new value of Group
+   */
+  private void setGroup (char newVar) {
+    Group = newVar;
+  }
+
+  /**
+   * Get the value of Group
+   * @return the value of Group
+   */
+  private char getGroup () {
+    return Group;
+  }
+
+  /**
+   * Set the value of Subgroup
+   * @param newVar the new value of Subgroup
+   */
+  private void setSubgroup (char newVar) {
+    Subgroup = newVar;
+  }
+
+  /**
+   * Get the value of Subgroup
+   * @return the value of Subgroup
+   */
+  private char getSubgroup () {
+    return Subgroup;
+  }
+
+  //
+  // Other methods
+  //
+
+}
